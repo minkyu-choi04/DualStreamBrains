298aad5227a536867bdbbd4e2ae5068f4ed24d3a
Minkyu Choi <minkyu.choi8904@gmail.com>
2024-02-06 14:34:49 -0500

    Update train.py
    Updated model parameter loading part.

--- train.py
+++ train.py
@@ -28,8 +28,7 @@ parser.add_argument('--density_ratio_D', default=2.5, type=float, help='Control 
 parser.add_argument('--grid_size_D', default=64, type=int, help='sampling resolution, w == h')
 parser.add_argument('--actmap_s', default=16, type=int, help='actmap size')
 
-parser.add_argument('--model_dir_coco', default='./model_parameter_coco.pt', type=str, help='model weights trained for MSCOCO')
-parser.add_argument('--model_dir_sal', default='./model_parameter_sal.pt', type=str, help='model wegiths trained for SALICON')
+parser.add_argument('--model_dir', default='./model_weights.pt', type=str, help='model weights')
 parser.add_argument('--image_size_h', default=174, type=int, help='input resoultion height')
 parser.add_argument('--image_size_w', default=336, type=int, help='input resoultion height')
 
@@ -43,41 +42,20 @@ def main():
     args.useStocFixs = False
     args.useIOR = False
     args.useWindow = True
-    train(args, model_dir_COCO, model_dir_sal, isFixedFixs, pad_mod)
+    train(args, model_dir, isFixedFixs, pad_mod)
 
 
-def train(args, model_dir_COCO, model_dir_sal, isFixedFixs, pad_mod, isRandomFixs=False):
+def train(args, model_dir, isFixedFixs, pad_mod, isRandomFixs=False):
     ## Agent model
     model = model_retina1d.CRNN_Model(args, isFixedFixs, isRandomFixs)
     model.apply(misc.initialize_weight)
 
-    
-    md_sal = torch.load(model_dir_sal)
-    md_coco = torch.load(model_dir_COCO)
-    fc_weight = {}
-    gru_weight = {}
-    retina_net_weight = {}
-    agent_net_weight = {}
-
-    modified_params = {}
-    for k, v in list(md_coco.items()):
-        k = k.replace('module.', '')
-        if 'gru' in k:
-            k = k.replace('gru.', '')
-            gru_weight[k] = v
-        elif 'fc' in k:
-            k = k.replace('fc.', '')
-            fc_weight[k] = v
-        elif 'retina_net' in k:
-            k = k.replace('retina_net.', '')
-            retina_net_weight[k] = v
-        else:
-            continue
-    model.fc.load_state_dict(fc_weight['fc'])
-    model.gru.load_state_dict(gru_weight['gru'])
-    model.retina_net.load_state_dict(retina_net_weight['retina_net'])
-    model.init_hidden = torch.nn.Parameter(md_coco['init_hidden'])
-    model.agent_net.load_state_dict(md_sal['agent_net'])
+    model_weights = torch.load(model_dir)
+    model.fc.load_state_dict(model_weights['fc'])
+    model.gru.load_state_dict(model_weights['gru'])
+    model.retina_net.load_state_dict(model_weights['retina_net'])
+    model.init_hidden = torch.nn.Parameter(model_weights['init_hidden'])
+    model.agent_net.load_state_dict(model_weights['agent_net'])
 
 
     ## Use GPU
